3a49d48e54f9178b124efc173e15cdfa926d6f21
joanhristov <joanhristov991@gmail.com>
2021-12-06 02:32:42 +0200

    removed test files

---
+++ AdoptPetsProject/Models/Home/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AdoptPetsProject.Models.Home
+{
+    public class IndexViewModel
+    {
+        public int TotalPets { get; init; }
+        public int TotalUsers { get; init; }
+
+        public int TotalAdoptions { get; init; }
+
+        public List<PetIndexViewModel> Pets { get; init; }
+    }
+}
---
+++ AdoptPetsProject/Models/Pets/PetListingViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace AdoptPetsProject.Models.Pets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class PetListingViewModel
+    {
+        public int Id { get; init; }
+
+        public string Breed { get; init; }
+
+        public string Name { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Age { get; init; }
+
+        public string Kind { get; init; }
+    }
+}
